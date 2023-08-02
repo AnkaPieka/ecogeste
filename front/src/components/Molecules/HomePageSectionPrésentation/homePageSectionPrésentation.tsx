4e87b4dc99814ce74ef939fd9a9722419efd0bbb
Ali <zaabat97@gmail.com>
2023-08-02 14:17:09 +0200

    initial commit

---
+++ front/src/components/Molecules/HomePageSectionPrésentation/homePageSectionPrésentation.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function HomePageSectionPresentation(){
+    return(
+        <div className="container">
+      {/* Texte écrit en dur */}
+    <h2>Are you ready to faces the challenges</h2>
+    <p>Join our echo-challenge app and participate in group challenges tant promotes
+        substainable actions to support the environment. Together, let's accomplish various eco-friendly
+        gestures and make a difference for a better world !
+    </p>
+<span> #Ecochallenge #SupportingTheEnvironnement</span>
+    </div>
+    )
+}
+export default HomePageSectionPresentation;
+
+
