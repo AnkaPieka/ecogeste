58e3390001c18f9724227d48bb8d4b71f906e1c7
Emi <emilef08@gmail.com>
2023-08-03 11:39:25 +0200

    first save

---
+++ back/src/tests/login.test.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+// import { User } from "../entity/User";
+// import { UserResolver } from "../resolver/UserResolver";
+// import dataSource from "../utils";
+import {describe, expect, it} from '@jest/globals';
+
+const calculate = (a: any, b: any) => {
+    return a + b;
+}
+
+describe('calculate', () => {
+    it('should calculate', () => {
+        const result = calculate(1, 1)
+        expect(result).toEqual(2)
+    })
+})
