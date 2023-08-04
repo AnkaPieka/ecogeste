2a6a32eab07dc9ba48794fc72535cd2fc7d4cb95
Ali <zaabat97@gmail.com>
2023-08-04 09:28:57 +0200

    WIP image implementation

--- front/src/components/Molecules/HomePageSectionPrésentation/homePageSectionPrésentation.tsx
+++ front/src/components/Molecules/HomePageSectionPrésentation/homePageSectionPrésentation.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-
+import logoFeuille from "../../../assets/logoFeuille.svg"
 function HomePageSectionPresentation(){
+
     return(
         <div className="container">
-      {/* Texte écrit en dur */}
-    <h2>Are you ready to faces the challenges</h2>
-    <p>Join our echo-challenge app and participate in group challenges tant promotes
+    <img src= {logoFeuille} alt="logoFeuille" className="logoImage" />
+    <h2 className="title">Are you <span className="highlighted">ready</span> to faces the challenges?</h2>
+    <div className="container-text">
+    <p className="description">Join our <span className="highlighted">echo-challenge app</span> and participate in group challenges tant promotes
         substainable actions to support the environment. Together, let's accomplish various eco-friendly
-        gestures and make a difference for a better world !
+        gestures and make a difference for a better world !<br />
     </p>
-<span> #Ecochallenge #SupportingTheEnvironnement</span>
+    <span className="hashtag"> #Ecochallenge #SupportingTheEnvironnement</span>
+    </div>
     </div>
     )
 }
