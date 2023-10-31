e36b80718ea741e52ddb3f1fb7ca4231e790f9f3
Anna Lefour <anka.piekarnik@gmail.com>
2023-10-31 10:43:27 +0100

    fix on userMutation test and workflow

--- testrunner/src/usermutation.test.ts
+++ testrunner/src/usermutation.test.ts
@@ -6,27 +6,28 @@ const {
   gql,
 } = require('@apollo/client/core');
 
-const fetch = require("cross-fetch");
-// import fetch from "cross-fetch";
+const fetch = require('cross-fetch');
 
 const client = new ApolloClient({
   link: new HttpLink({
-
     uri: 'http://localhost:4000/',
-
     fetch,
   }),
   cache: new InMemoryCache(),
 });
 
-
 const CREATE_USER = gql`
-  mutation Mutation($password: String!, $email: String!, $name: String!) {
-    createUser(password: $password, email: $email, name: $name) {
-
-      email,
-      name,
+  mutation Mutation(
+    $password: String!
+    $email: String!
+    $name: String!
+    $avatar: String!
+  ) {
+    addUser(password: $password, email: $email, name: $name, avatar: $avatar) {
+      email
+      name
       password
+      avatar
     }
   }
 `;
@@ -35,13 +36,20 @@ describe('User resolver', () => {
   it('create user', async () => {
     const res = await client.mutate({
       mutation: CREATE_USER,
-      variables: { name: 'testName', email: 'test@test.com', password: 'test' },
+      variables: {
+        name: 'testName',
+        email: 'test@test.com',
+        password: 'test',
+        avatar: 'avatar',
+      },
     });
 
-    expect(res.data?.createUser).toEqual({
+    expect(res.data?.addUser).toEqual({
+      __typename: 'User',
       name: 'testName',
       email: 'test@test.com',
+      password: 'test',
+      avatar: 'avatar',
     });
-
   });
 });
