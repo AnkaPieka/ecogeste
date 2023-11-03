5a09344fa3055d510549de56d9af39bb291dcea0
Anna Lefour <anka.piekarnik@gmail.com>
2023-11-03 14:10:47 +0100

    cleaning and fixing

--- front/src/Pages/HomePage.tsx
+++ front/src/Pages/HomePage.tsx
@@ -2,12 +2,14 @@ import HomePageSectionPresentation from '../components/Molecules/HomePageSection
 
 import NavBar from '../components/Organisms/NavBar';
 import Footer from '../components/Organisms/Footer/Footer';
+import OpinionsCarousel from '../components/Organisms/OpinionsCarousel/OpinionsCarousel';
 
 const HomePage = () => {
   return (
     <div>
       <NavBar />
       <HomePageSectionPresentation></HomePageSectionPresentation>
+      {/* <OpinionsCarousel></OpinionsCarousel> */}
       <Footer />
     </div>
   );
