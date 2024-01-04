0d9a36d3f232ba2629c8118144c496305273ee36
Ali <zaabat97@gmail.com>
2024-01-04 17:33:26 +0100

    front implementation on progress

--- front/src/components/Organisms/ChallengeDashboard/ChallengeDashboard.tsx
+++ front/src/components/Organisms/ChallengeDashboard/ChallengeDashboard.tsx
@@ -1,5 +1,6 @@
 import { gql, useQuery  } from "@apollo/client";
 import Challenge from "./Challenge.interface";
+import handimage from "../../../assets/handimage.png"
 
 const GET_CHALLENGES = gql`
 query GetAllChallenges {
@@ -20,12 +21,31 @@ query GetAllChallenges {
 const ChallengeDashboard = () => {
     const {data} = useQuery(GET_CHALLENGES);
     return (
-    <div className="challenge-container">
-        <h1>My Challenge</h1>
-        {data && data.getAllChallenges.map((challenge: Challenge) => (
-        <p key={challenge.id}>{challenge.title}</p>
+<div className="challenge-container">
+  <h1 className="challenge-title">My Challenge</h1>
+  <div className="challenge-content">
+    {data && data.getAllChallenges.map((challenge: Challenge) => (
+      <div key={challenge.id}>
+        <h6>{challenge.title}</h6>
+        <img src={challenge.photo} alt="Challenge" />
+      </div>
     ))}
-    </div>
+  </div>
+  {/* <img className= "hand" src={handimage} alt="logo" /> */}
+</div>
+
     )
 }
 export default ChallengeDashboard
+
+/*
+Exemple de mutation avec une image
+{  "duration": "One hour",
+  "creator": 15,
+  "likes": 150,
+  "photo": "https://images.pexels.com/lib/api/pexels.png",
+  "progress": 10,
+  "title": "Web Eco Challenge",
+  "description": "Try to reduce your energy try to reduce your energy consumption"
+}
+*/
