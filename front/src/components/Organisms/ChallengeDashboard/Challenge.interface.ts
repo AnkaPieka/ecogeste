504bfe7cda3e1b25b6db1ad2548b65b76426bbc5
Ali <zaabat97@gmail.com>
2024-01-04 15:49:35 +0100

    display data from DB done

---
+++ front/src/components/Organisms/ChallengeDashboard/Challenge.interface.ts
@@ -0,0 +1,11 @@
+interface Challenge {
+    creator: string;
+    description: string;
+    duration: string;
+    id: number;
+    likes: number;
+    photo: string;
+    progress: number;
+    title: string;
+  }
+  export default Challenge;
--- front/src/components/Organisms/ChallengeDashboard/ChallengeDashboard.tsx
+++ front/src/components/Organisms/ChallengeDashboard/ChallengeDashboard.tsx
@@ -1,9 +1,30 @@
+import { gql, useQuery  } from "@apollo/client";
+import Challenge from "./Challenge.interface";
+
+const GET_CHALLENGES = gql`
+query GetAllChallenges {
+    getAllChallenges {
+    creator
+      description
+      duration
+      id
+      likes
+      photo
+      progress
+      title
+    }
+  }
+`;
+
 
 const ChallengeDashboard = () => {
+    const {data} = useQuery(GET_CHALLENGES);
     return (
     <div className="challenge-container">
         <h1>My Challenge</h1>
-        <p>Coucou Maurice</p>
+        {data && data.getAllChallenges.map((challenge: Challenge) => (
+        <p key={challenge.id}>{challenge.title}</p>
+    ))}
     </div>
     )
 }
