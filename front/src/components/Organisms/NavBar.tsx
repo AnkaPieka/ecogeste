63c21d6cda7ed28d5b79e546c3525169699397d8
Anna Lefour <anka.piekarnik@gmail.com>
2024-01-05 11:44:41 +0100

    Finished comments on homepage

--- front/src/components/Organisms/NavBar.tsx
+++ front/src/components/Organisms/NavBar.tsx
@@ -1,19 +1,19 @@
-import logo from '../../assets/logoecogeste.png'
+import logo from '../../assets/logoecogeste.png';
 import ButtonLogin from '../Atoms/ButtonLogin';
 import ButtonSignUp from '../Atoms/ButtonSignUp';
 
- const NavBar = () =>{
+const NavBar = () => {
   return (
     <div className="navBar">
-        <div className="logo">
-            <img src={logo} alt="logo" />
-        </div>
-        <div className="btnsNav">
-            <ButtonLogin />
-            <ButtonSignUp />
-        </div>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="btnsNav">
+        <ButtonLogin />
+        <ButtonSignUp />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default NavBar;
--- front/src/components/Organisms/OpinionsCarousel/OpinionsCarousel.tsx
+++ front/src/components/Organisms/OpinionsCarousel/OpinionsCarousel.tsx
@@ -1,42 +1,58 @@
 import React from 'react';
 import OpinionCard from '../../Molecules/OpinionCard/OpinionCard';
 import ArrowButton from '../../Atoms/ArrowButton/ArrowButton';
+import Mike from '../../../assets/james-denton.jpg';
+import Percy from '../../../assets/Logan_Lerman.jpg';
+import Shakira from '../../../assets/shakira.jpg';
 
 import './OpinionsCarousel.scss';
 
 type OpinionsCarouselProps = {};
 
 const OpinionsCarousel = ({}: OpinionsCarouselProps) => {
-  let mockData = [
-    { pic: '', name: 'Mike', opinion: 'lorem ipsum dolor sit amet' },
-    { pic: '', name: 'Percy', opinion: 'lorem ipsum dolor sit amet' },
-    { pic: '', name: 'Shakira', opinion: 'lorem ipsum dolor sit amet' },
+  let mockData: { pic: string; name: string; opinion: string }[] = [
+    {
+      pic: Mike,
+      name: 'Mike',
+      opinion: 'A nice moment between friends! Thx Ecogestes',
+    },
+    {
+      pic: Percy,
+      name: 'Percy',
+      opinion: 'Helped me and my family develop new good habits',
+    },
+    {
+      pic: Shakira,
+      name: 'Shakira',
+      opinion: 'I saw an improvement in my teams relations',
+    },
   ];
+
   return (
     <div id="opinionCarousel">
-      <ArrowButton
+      {/* <ArrowButton
         direction="back"
         onClick={function (): void {
           throw new Error('Function not implemented.');
         }}
-      />
+      /> */}
 
       <div className="opinions">
         {mockData.map((data) => {
           return (
-            <OpinionCard title={data.name} profilePictureSrc={''}>
+            <OpinionCard title={data.name} profilePictureSrc={data.pic}>
               {data.opinion}
             </OpinionCard>
           );
         })}
       </div>
 
-      <ArrowButton
+      {/* <ArrowButton
         direction="forward"
         onClick={function (): void {
           throw new Error('Function not implemented.');
         }}
-      />
+      /> */}
     </div>
   );
 };
